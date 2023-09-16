7a891f168ae613d126ad1264bf0b6424be52d0d4
Willian Wallace <128304786+Willianbrasil1213@users.noreply.github.com>
2023-09-16 13:14:04 -0300

    Contatos.java

---
+++ Contatos.java
@@ -0,0 +1,32 @@
+public class Contatos {
+    
+    String nome;
+    String sexo;
+    double telefone;
+    String email;
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getSexo() {
+        return sexo;
+    }
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+    public double getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(double telefone) {
+        this.telefone = telefone;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
